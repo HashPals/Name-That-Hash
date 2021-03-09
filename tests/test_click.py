9ec950bcdcdc36b2cd985bcc5e173c4a53cb791f
brandon <brandonskerritt51@gmail.com>
2021-03-09 13:49:06 +0000

    Fixed base64 issue

--- tests/test_click.py
+++ tests/test_click.py
@@ -49,3 +49,8 @@ def test_file_input():
     result = runner.invoke(main, ["-f", "tests/mocks/hashes.txt", '-b64', '-g'])
     assert "SHA-1" in result.output
     assert "SHA-512" in result.output
+
+def test_file_input():
+    runner = CliRunner()
+    result = runner.invoke(main, ["-t", "$krb5tgs$23$*user$realm$test/spn*$63386d22d359fe42230300d56852c9eb$891ad31d09ab89c6b3b8c5e5de6c06a7f49fd559d7a9a3c32576c8fedf705376cea582ab5938f7fc8bc741acf05c5990741b36ef4311fe3562a41b70a4ec6ecba849905f2385bb3799d92499909658c7287c49160276bca0006c350b0db4fd387adc27c01e9e9ad0c20ed53a7e6356dee2452e35eca2a6a1d1432796fc5c19d068978df74d3d0baf35c77de12456bf1144b6a750d11f55805f5a16ece2975246e2d026dce997fba34ac8757312e9e4e6272de35e20d52fb668c5ed"])
+    assert "Kerberos" in result.output
